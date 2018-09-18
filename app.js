662b34b782bcdbcccd13e085d4ebd7347f4ab82e
Michelle Torres <michelle.torres@corb.mx>
2018-09-18 08:32:23 -0500

    Routes by modules

--- app.js
+++ app.js
@@ -1,62 +1,9 @@
 const express = require('express');
 
-const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const router = require('./routes');
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/users', (req, res) => {
-  const users = [
-    {
-      id: 1,
-      name: 'juan',
-      email: 'juan@correo',
-    },
-    {
-      id: 2,
-      name: 'juan2',
-      email: 'juan2@correo',
-    },
-  ];
-
-  const json = {
-    response: 'ok',
-    data: users,
-    total: 2,
-  };
-
-  res.send(json);
-});
-
-app.get('/users/:userId', (req, res) => {
-  const user = {
-    id: req.params.userId,
-    name: `juan${req.params.userId}`,
-    email: `juan${req.params.userId}@correo`,
-  };
-
-  res.send(user);
-});
-
-app.post('/users', (req, res) => {
-  console.log(req.body);
-  const json = {
-    response: 'ok',
-    data: {
-      id: 100,
-      name: req.body.name,
-    },
-  };
-
-  res.send(json);
-});
-
-app.put('/users/:userId', (req, res) => {
-  res.send('editado lindo');
-});
+app.use(router);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
